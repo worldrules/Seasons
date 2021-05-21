49815ff3c60094c089d1351b537e00a263c0c8f7
worldrules <worldrulesdev@gmail.com>
2021-05-21 20:35:11 -0300

    troca para class app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position)=>console.log(position),
-        (err) =>console.log(err)
-        
-    );
-
-    return (
-        
-        <div className="content">Latitude: </div>
-    );
-};
+class App extends React.Component {
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>console.log(position),
+            (err) =>console.log(err)
+            
+        );
+        return <div className="content">Latitude:</div>
+    }
+}
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
