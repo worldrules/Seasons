bb3aaedbd24f810f9750f35d8d505f7075cf54d9
worldrules <worldrulesdev@gmail.com>
2021-05-29 03:37:28 -0300

    keke

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function SeasonDisplay() {
+
+const getSeason = (lat, month) => {
+    if (month > 2 &&  month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'winter' : 'summer';
+    }
+}
+
+export default function SeasonDisplay(props) {
+    
+    const season = getSeason(props.lat, new Date().getMonth());
+
     return (
         <div>
-            Season Display
+            {season}
         </div>
     )
 }
